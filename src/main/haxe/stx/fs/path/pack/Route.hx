1c00edbbf0e343e13cd7163c9e52ffadbc4b7db6
0b1kn00b <polysemantic@gmail.com>
2022-03-01 09:24:52 +0100

    RouteLift

--- src/main/haxe/stx/fs/path/pack/Route.hx
+++ src/main/haxe/stx/fs/path/pack/Route.hx
@@ -5,6 +5,7 @@ package stx.fs.path.pack;
 **/
 typedef RouteDef = Cluster<Move>;
 
+@:using(stx.fs.path.pack.Route.RouteLift)
 @:forward(lfold) abstract Route(RouteDef) from RouteDef to RouteDef{
   public function new(self) this = self;
   static public function lift(self:RouteDef):Route return new Route(self);
@@ -29,4 +30,7 @@ typedef RouteDef = Cluster<Move>;
   public function prj():RouteDef return this;
   private var self(get,never):Route;
   private function get_self():Route return lift(this);
+}
+class RouteLift{
+  
 }
