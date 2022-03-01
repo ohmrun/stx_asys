37e70994d518e7916ca8c25a31acdf3c930dc3b6
0b1kn00b <polysemantic@gmail.com>
2022-03-01 09:23:56 +0100

    Entry.fromToken

--- src/main/haxe/stx/fs/path/pack/Entry.hx
+++ src/main/haxe/stx/fs/path/pack/Entry.hx
@@ -7,6 +7,7 @@ typedef EntryDef = {
 /**
   An entry in a `Directory`, i.e the filename and extension
 **/
+@:using(stx.fs.path.pack.Entry.EntryLift)
 @:forward abstract Entry(EntryDef) from EntryDef to EntryDef{
   public function new(self) this = self;
   static public function lift(self:EntryDef):Entry return new Entry(self);
@@ -42,4 +43,24 @@ typedef EntryDef = {
   private var self(get,never):Entry;
   private function get_self():Entry return lift(this);
 
+  static public function fromToken(self:Token):Option<Entry>{
+    return switch(self){
+      case FPTDrive(name)     : None;
+      case FPTRel             : None;
+      case FPTUp              : None;
+      case FPTSep             : None;
+      case FPTDown(str)       : 
+        final arr = str.split(".");
+        if(arr.length > 1){
+          final ext = arr.pop();
+          Some(Entry.make(arr.join("."),ext));
+        }else{
+          Some(Entry.make(str,null));
+        }
+      case FPTFile(str,ext)   : Some(Entry.make(str,ext));
+    }
+  }
+}
+class EntryLift{
+  
 }
