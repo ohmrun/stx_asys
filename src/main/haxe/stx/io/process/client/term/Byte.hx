f53a65d28d55811ab31ff8e32d0c5210cefe3f80
0b1kn00b <polysemantic@gmail.com>
2022-11-14 14:04:36 +0100

    Byte

---
+++ src/main/haxe/stx/io/process/client/term/Byte.hx
@@ -0,0 +1,24 @@
+package stx.io.process.client.term;
+
+/**
+  Requires all the bytes from the stdout of a process.
+**/
+abstract Reply(ProcessClientDef<Bytes>) from ProcessClientDef<Bytes> to ProcessClientDef<Bytes>{
+  public function new(?error=false){
+    this = __.await(
+      PReqInput(IReqValue(I8),error),
+      cat
+    );
+  }
+  static public function cat(res:ProcessResponse):ProcessClientDef<Bytes>{
+    __.log().debug('$res');
+    return switch(res){
+      case PResValue(Failure(IResBytes(bytes))) : __.ended(End(__.fault().of(E_Process_Raw(bytes))));
+      case PResValue(Success(IResBytes(bytes))) : __.ended(Val(bytes));
+      case PResError(raw)                       : __.ended(End(raw));
+      case PResOffer(req)                       : __.await(req,cat);
+      case PResState(state)                     : __.ended(End(__.fault().of(E_ProcessState(state))));
+      default                                   : __.ended(End(__.fault().of(E_Process_Unsupported('$res'))));
+    }
+  }
+}
